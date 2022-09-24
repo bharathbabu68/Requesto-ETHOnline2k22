a423806a72b119951d3e5c14cff6e3dbced27bc7
Bharath Babu <bharathbabu68@gmail.com>
2022-09-24 23:50:04 +0400

    fix for deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,7 +9,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <StrictMode>
     <App />
+    </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
